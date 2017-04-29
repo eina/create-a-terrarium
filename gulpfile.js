67579cbacf117c123db75a082f29e14464239a83
thelittleblacksmith <einers@outlook.com>
2017-04-28 20:58:49 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp'),
+  plumber = require('gulp-plumber'),
+  rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-cssnano');
+var sass = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('styles', function () {
+  gulp.src(['./src/styles/**/*.scss', '!src/styles/**/*.min.css'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('scripts', function () {
+  return gulp.src(['./src/scripts/**/*.js', '!src/scripts/**/*.min.js'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('minifyhtml', function () {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('src/img/**/*')
+    .pipe(gulp.dest('dist/img'));
+})
+
+gulp.task('default', ['browser-sync', 'images'], function () {
+  gulp.watch("./src/styles/**/*.scss", ['styles']);
+  gulp.watch("./src/scripts/**/*.js", ['scripts']);
+  gulp.watch("./dist/*.html", ['bs-reload']);
+});
